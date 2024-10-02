57337b9b7385894be297e702cb6c15f1118fa7d7
Zakhy <lirchzakhar@gmail.com>
2024-10-02 20:50:56 +0700

    admin reg models

--- kitten/admin.py
+++ kitten/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from kitten.models.breed import Breed
+from kitten.models.kitten import Kitten
+from kitten.models.rating import Rating
+
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Kitten)
+class KittenAdmin(admin.ModelAdmin):
+    pass
