f6f2c4e3795a6fb25f1af879444f5db1a9ddca61
Zakhy <lirchzakhar@gmail.com>
2024-10-05 12:42:55 +0700

    sort by breed

--- app/v1/views/kitten.py
+++ app/v1/views/kitten.py
@@ -1,10 +1,10 @@
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets, permissions
+from drf_yasg import openapi
+from rest_framework import viewsets
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from v1.models.kitten import Kitten
-from v1.schema import jwt_auth
 from v1.serializers.kitten import KittenSerializer
 
 
@@ -12,24 +12,44 @@ class KittenViewSet(viewsets.ModelViewSet):
     queryset = Kitten.objects.all()
     serializer_class = KittenSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    @swagger_auto_schema(manual_parameters=[jwt_auth])
+
+    # Параметр для фильтрации по породе
+    breed_param = openapi.Parameter(
+        'breed',
+        openapi.IN_QUERY,
+        description="Filter kittens by breed",
+        type=openapi.TYPE_INTEGER
+    )
+
+    # Параметр для передачи JWT токена в заголовке
+    jwt_auth_param = openapi.Parameter(
+        'Authorization',
+        openapi.IN_HEADER,
+        description="JWT Authorization token",
+        type=openapi.TYPE_STRING,
+        required=True,
+        default="Bearer <your_token>"
+    )
+
+    @swagger_auto_schema(manual_parameters=[jwt_auth_param, breed_param])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(manual_parameters=[jwt_auth_param])
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
 
-    @swagger_auto_schema(manual_parameters=[jwt_auth])
+    @swagger_auto_schema(manual_parameters=[jwt_auth_param])
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
 
-    @swagger_auto_schema(manual_parameters=[jwt_auth])
+    @swagger_auto_schema(manual_parameters=[jwt_auth_param])
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
 
-    @swagger_auto_schema(manual_parameters=[jwt_auth])
+    @swagger_auto_schema(manual_parameters=[jwt_auth_param])
     def partial_update(self, request, *args, **kwargs):
         return super().partial_update(request, *args, **kwargs)
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
 
     def get_queryset(self):
         breed_id = self.request.query_params.get('breed', None)
@@ -37,15 +57,16 @@ class KittenViewSet(viewsets.ModelViewSet):
             return Kitten.objects.filter(breed_id=breed_id)
         return Kitten.objects.all()
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
     def perform_update(self, serializer):
-        # Запрещаем изменять данные о котенке, если пользователь не является владельцем
         instance = self.get_object()
         if instance.owner != self.request.user:
             raise PermissionDenied("You cannot edit this kitten.")
         serializer.save()
 
     def perform_destroy(self, instance):
-        # Запрещаем удалять котенка, если пользователь не является владельцем
         if instance.owner != self.request.user:
             raise PermissionDenied("You cannot delete this kitten.")
         instance.delete()
